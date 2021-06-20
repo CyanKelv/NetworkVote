a19d098e4a7de1f72b5e432331ca315d3bc7c91a
Kelvin <jetixng@yahoo.com>
2021-06-20 14:50:14 +0800

    fix variable type name and query condition

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -14,19 +14,19 @@ SELECT name, servers, lastupdated FROM votes WHERE name = :name;
 -- #    }
 
 -- #    { get_vote_by_date
--- #      :timestamp integer
+-- #      :timestamp int
 SELECT name, servers, lastupdated FROM votes WHERE lastupdated < :timestamp;
 -- #    }
 
 -- #    { update_vote
 -- #      :name string
 -- #      :servers string
--- #      :timestamp integer
-INSERT OR REPLACE INTO votes(name, servers, lastupdated) VALUES (:name, :servers, :timestamp);
+-- #      :timestamp int
+REPLACE INTO votes(name, servers, lastupdated) VALUES (:name, :servers, :timestamp);
 -- #    }
 
 -- #    { delete_vote
 -- #      :name string
-DELETE FROM votes where name = :name;
+DELETE FROM votes WHERE name = :name;
 -- #    }
 -- #}
